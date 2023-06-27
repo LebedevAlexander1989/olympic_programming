c67ce27d0f036534703ba1866ff5c39c2ad71f9c
79855 <leb.alex123@gmail.com>
2023-06-27 19:50:02 +0300

    acmp312

--- acmp312/src/Main.java
+++ acmp312/src/Main.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 
 public class Main {
     void solve(Scanner in, PrintWriter out) {
-
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int n = in.nextInt();
+        int d = a2 - a1;
+        out.println(a1 + d * (n -1));
     }
 
     void run() {
