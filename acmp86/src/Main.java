5e925e957d5702f501c40615f6a72694c055ccb5
79855 <leb.alex123@gmail.com>
2023-06-27 19:57:16 +0300

    acmp86

---
+++ acmp86/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    void solve(Scanner in, PrintWriter out) {
+        int n = in.nextInt();
+        out.println(n * n - 3 * (n - 1) - 1);
+    }
+
+    void run() {
+        try (Scanner in = new Scanner(System.in);
+             PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }}
