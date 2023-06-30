58515217ac057ed79f258f65b84e13c46f3570a5
79855 <leb.alex123@gmail.com>
2023-06-30 17:00:04 +0300

    acmp1110

---
+++ acmp1110/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    void solve(Scanner in, PrintWriter out) {
+        String number = in.nextLine();
+        out.println(number.charAt(number.length() - 1));
+    }
+
+    void run() {
+        try (Scanner in = new Scanner(System.in);
+             PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}
